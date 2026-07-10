b400d93f870d3655c374100f04d3d494ce560f3d
MahendranDM <mahendrandm2003@gmail.com>
2026-07-10 18:51:42 +0530

    Updated NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,23 @@
-
-        import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav
-      className="navbar navbar-expand-lg navbar-dark bg-dark shadow"
-      style={{ fontFamily: "Poppins, sans-serif" }}
+      className="navbar navbar-expand-lg shadow-lg"
+      style={{
+        background: "linear-gradient(90deg, #0f766e, #14b8a6)",
+       fontFamily: "'Nunito', sans-serif",
+        padding: "15px 0",
+      }}
     >
       <div className="container">
 
         <button
-          className="navbar-toggler"
+          className="navbar-toggler border-0"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -28,36 +28,36 @@ const NavBar = () => {
         >
           <ul className="navbar-nav">
 
-            <li className="nav-item mx-3">
-              <Link className="nav-link text-white fw-bold fs-5" to="/">
-                Patients
+            <li className="nav-item mx-4">
+              <Link className="nav-link text-white fw-bold fs-4" to="/">
+                🏥 Patients
               </Link>
             </li>
 
-            <li className="nav-item mx-3">
-              <Link className="nav-link text-white fw-bold fs-5" to="/add">
-                Add Patient
+            <li className="nav-item mx-4">
+              <Link className="nav-link text-white fw-bold fs-4" to="/add">
+                ➕ Add Patient
               </Link>
             </li>
 
-            <li className="nav-item mx-3">
-              <Link className="nav-link text-white fw-bold fs-5" to="/search">
-                Search Patient
+            <li className="nav-item mx-4">
+              <Link className="nav-link text-white fw-bold fs-4" to="/search">
+                🔍 Search Patient
               </Link>
             </li>
 
-            <li className="nav-item mx-3">
-              <Link className="nav-link text-white fw-bold fs-5" to="/discharge">
-                Discharge Patient
+            <li className="nav-item mx-4">
+              <Link className="nav-link text-white fw-bold fs-4" to="/discharge">
+                🚑 Discharge
               </Link>
             </li>
 
           </ul>
         </div>
+
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
-  
+export default NavBar;
